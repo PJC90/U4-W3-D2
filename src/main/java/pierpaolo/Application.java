c372db510e49326117b0096e76d22918d4e0b157
Pierpaolo Colasante <colasantepierpaolo@gmail.com>
2023-12-12 15:47:11 +0100

    EventoDAO

--- src/main/java/pierpaolo/Application.java
+++ src/main/java/pierpaolo/Application.java
@@ -1,9 +1,30 @@
 package pierpaolo;
 
+import pierpaolo.dao.EventoDAO;
+import pierpaolo.entities.Evento;
+import pierpaolo.entities.EventoType;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class Application {
-
+    private static final EntityManagerFactory emf = Persistence.createEntityManagerFactory("u4w3d2jpa");
     public static void main(String[] args) {
+        EntityManager em = emf.createEntityManager();
+        EventoDAO ed = new EventoDAO(em);
 
-        System.out.println("Hello World!");
+        Evento battesimo = new Evento("Pippo","02-05-2024","Battesimo", EventoType.PUBBLICO,200);
+//----------------------------------------------SAVE
+//        ed.save(battesimo);
+// ---------------------------------------------FIND BY ID
+        long id = 1;
+        Evento battesimoFindById = ed.findById(id);
+        if(battesimoFindById != null){
+            System.out.println(battesimoFindById);
+        } else {
+            System.out.println("Evento di ID " + id + " non trovato");
+        }
+
     }
 }
--- src/main/java/pierpaolo/dao/EventoDAO.java
+++ src/main/java/pierpaolo/dao/EventoDAO.java
@@ -1,4 +1,28 @@
 package pierpaolo.dao;
 
+import pierpaolo.entities.Evento;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityTransaction;
+
 public class EventoDAO {
+    private final EntityManager em; // Tutti i metodi qua sotto avranno bisogno dell'EntityManager poichè dovranno interagire col DB
+
+    public EventoDAO(EntityManager em) {
+        this.em = em;
+    }
+
+    public void save(Evento evento){
+        EntityTransaction transaction = em.getTransaction();
+        transaction.begin();
+        em.persist(evento);
+        transaction.commit();
+        System.out.println("Evento " + evento.getTitolo() + " generato!");
+    }
+
+    public Evento findById(long id){
+        // SELECT * FROM students WHERE id = 1
+        return em.find(Evento.class, id);
+        // Primo parametro è l'Entity, secondo l'id da ricercare
+    }
 }
